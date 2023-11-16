4bbe172bb12d9e71bc52ee466d8fb523a71565eb
DamianCoventry <59074625+DamianCoventry@users.noreply.github.com>
2023-11-16 20:19:49 +1300

    Annotated two columns

--- Models/Restaurant.cs
+++ Models/Restaurant.cs
@@ -1,11 +1,15 @@
-﻿namespace auckland_curry_movement_api.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace auckland_curry_movement_api.Models
 {
     public class Restaurant
     {
         public int? ID { get; set; }
         public string Name { get; set; } = string.Empty;
+        [Column("Street Address")]
         public string? StreetAddress { get; set; }
         public string Suburb { get; set; } = string.Empty;
+        [Column("Phone Number")]
         public string? PhoneNumber { get; set; }
         public bool IsArchived { get; set; }
         public string ArchiveReason { get; set; } = string.Empty;
